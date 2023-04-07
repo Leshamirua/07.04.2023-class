ba4d0daa8647d28fe6c9ec5ee099d9a3747401f0
Leshamir <anastasy34@gmail.com>
2023-04-08 01:12:00 +0300

    Добавьте файлы проекта.

---
+++ 07.04.2023 class/Form1.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Threading;
+
+namespace _07._04._2023_class
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void richTextBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            StreamReader read = new StreamReader("../../1.txt");
+            string text = read.ReadToEnd();
+            progressBar1.Minimum = 0;
+            progressBar1.Maximum = 1000;
+            progressBar1.Value = 0;
+            string[] words = text.Split(new[] { ' '}, StringSplitOptions.RemoveEmptyEntries);
+            int numWords = words.Length;
+            foreach (string word in words)
+            {
+                richTextBox1.AppendText(" " + word);
+
+            }
+            int progressBarValue = numWords;
+            progressBar1.Value = progressBarValue;
+            read.Close();
+            
+            
+
+        }
+    }
+}
